752f6f420e42ee9fc54050fe0ddc1a629aa87333
Naufal Zaidan <naufal.zaidan@mhs.itenas.ac.id>
2023-12-15 01:27:13 +0700

    1

--- main.cpp
+++ main.cpp
@@ -592,8 +592,8 @@ int main() {
 		
 		/* MOON */
 		glm::mat4 model_moon;
-		xx = sin(glfwGetTime() * PlanetSpeed * 67.55f) * 100.0f * 0.5f *1.3f;
-		zz = cos(glfwGetTime() * PlanetSpeed * 67.55f) * 100.0f * 0.5f *1.3f;
+		xx = sin(glfwGetTime() * PlanetSpeed * 7.55f) * 100.0f * 0.5f *1.3f;
+		zz = cos(glfwGetTime() * PlanetSpeed * 7.55f) * 100.0f * 0.5f *1.3f;
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture_moon);
 		model_moon = glm::rotate(model_moon, glm::radians(SceneRotateY), glm::vec3(1.0f, 0.0f, 0.0f));
@@ -1066,49 +1066,49 @@ unsigned int loadTexture(char const * path)
 
 	return textureID;
 }
-void RenderText(Shader &s, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
-{
-	// Activate corresponding render state	
-	s.Use();
-	glUniform3f(glGetUniformLocation(s.ID, "textColor"), color.x, color.y, color.z);
-	glActiveTexture(GL_TEXTURE0);
-	glBindVertexArray(textVAO);
-
-	// Iterate through all characters
-	std::string::const_iterator c;
-	for (c = text.begin(); c != text.end(); c++)
-	{
-		Character ch = Characters[*c];
-
-		GLfloat xpos = x + ch.Bearing.x * scale;
-		GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
-
-		GLfloat w = ch.Size.x * scale;
-		GLfloat h = ch.Size.y * scale;
-		// Update VBO for each character
-		GLfloat vertices[6][4] = {
-			{ xpos,     ypos + h,   0.0, 0.0 },
-			{ xpos,     ypos,       0.0, 1.0 },
-			{ xpos + w, ypos,       1.0, 1.0 },
-
-			{ xpos,     ypos + h,   0.0, 0.0 },
-			{ xpos + w, ypos,       1.0, 1.0 },
-			{ xpos + w, ypos + h,   1.0, 0.0 }
-		};
-		// Render glyph texture over quad
-		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
-		// Update content of VBO memory
-		glBindBuffer(GL_ARRAY_BUFFER, textVBO);
-		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		// Render quad
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		// Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-		x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64)
-	}
-	glBindVertexArray(0);
-	glBindTexture(GL_TEXTURE_2D, 0);
-}
+//void RenderText(Shader &s, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
+//{
+//	// Activate corresponding render state	
+//	s.Use();
+//	glUniform3f(glGetUniformLocation(s.ID, "textColor"), color.x, color.y, color.z);
+//	glActiveTexture(GL_TEXTURE0);
+//	glBindVertexArray(textVAO);
+//
+//	// Iterate through all characters
+//	std::string::const_iterator c;
+//	for (c = text.begin(); c != text.end(); c++)
+//	{
+//		Character ch = Characters[*c];
+//
+//		GLfloat xpos = x + ch.Bearing.x * scale;
+//		GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+//
+//		GLfloat w = ch.Size.x * scale;
+//		GLfloat h = ch.Size.y * scale;
+//		// Update VBO for each character
+//		GLfloat vertices[6][4] = {
+//			{ xpos,     ypos + h,   0.0, 0.0 },
+//			{ xpos,     ypos,       0.0, 1.0 },
+//			{ xpos + w, ypos,       1.0, 1.0 },
+//
+//			{ xpos,     ypos + h,   0.0, 0.0 },
+//			{ xpos + w, ypos,       1.0, 1.0 },
+//			{ xpos + w, ypos + h,   1.0, 0.0 }
+//		};
+//		// Render glyph texture over quad
+//		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+//		// Update content of VBO memory
+//		glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+//		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+//		glBindBuffer(GL_ARRAY_BUFFER, 0);
+//		// Render quad
+//		glDrawArrays(GL_TRIANGLES, 0, 6);
+//		// Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+//		x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64)
+//	}
+//	glBindVertexArray(0);
+//	glBindTexture(GL_TEXTURE_2D, 0);
+//}
 
 //void ShowInfo(Shader &s)
 //{
